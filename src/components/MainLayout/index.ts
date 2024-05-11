af74f63b3524179c238ce36a48deaecb6ffeab05
Rodrigo Rocha <rocha.webdev@gmail.com>
2024-05-11 17:22:20 -0300

    feat: create MainLayout component and configure render

---
+++ src/components/MainLayout/index.ts
@@ -0,0 +1,44 @@
+import { css, html } from "iares";
+
+import { SidebarUi } from "@/components/SidebarUi";
+
+const template = () => html`
+<div class="sidebar-ctx">
+  <${SidebarUi} />
+</div>
+<div class="header-ctx">header</div>
+<div class="content-ctx">
+  <router-view></router-view>
+</div>
+`
+
+export const MainLayout = () => {
+  return {
+    template,
+    styles
+  }
+}
+
+const styles = () => css`
+  main-layout {
+    display:grid;
+    grid-template-columns: 250px 1fr;
+    grid-template-rows: 75px 1fr;
+    grid-template-areas: "sidebar header" "sidebar content";
+    width: 100%;
+  }
+
+  .header-ctx {
+    grid-area: header;
+  }
+
+  .sidebar-ctx {
+    grid-area: sidebar;
+  }
+
+  .content-ctx {
+    grid-area: content;
+  }
+
+  
+`
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import environment from "env";
 import { createApp, html, render, router } from "iares";
 
-import { AppMain } from "@/components/AppMain";
+import { MainLayout } from "@/components/MainLayout";
 import { routes } from "./routes";
 
 export const appHost = createApp({
   onMount(context, props) {
-    render(html`<${AppMain} />`);
+    render(html`<${MainLayout} />`);
     router({ routes, context }).init();
   },
 });

15c5efdbf2a7fe94ce32421003bf816d59c81f8e
Rodrigo Rocha <rocha.webdev@gmail.com>
2024-05-14 11:37:35 -0300

    style: improve code style

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,16 @@
-import environment from "env";
+import { environment } from "env";
 import { createApp, html, render, router } from "iares";
-
 import { MainLayout } from "@/components/MainLayout";
 import { routes } from "./routes";
 
 export const appHost = createApp({
-  onMount(context, props) {
+  onMount(context) {
     render(html`<${MainLayout} />`);
     router({ routes, context }).init();
   },
 });
 
 if (environment === "development") {
-  new EventSource("esbuild").addEventListener("change", () => {
-    window.location.reload();
-  });
+  new EventSource('/esbuild').addEventListener('change', () => location.reload());
 }
+
